f23c2941602e42ca92c2905982696c6d800f345d
arunmahara <arunmahara124@gmail.com>
2023-04-04 00:38:05 +0545

    feat(user): api to list details of user

--- post_real/apps/users/urls.py
+++ post_real/apps/users/urls.py
@@ -1,6 +1,6 @@
-from django.urls import include, path
-from rest_framework import routers
 from . import views
+from rest_framework import routers
+from django.urls import include, path
 
 router = routers.DefaultRouter()
 # router.register(r'users', views.UserViewSet, basename="users")
@@ -9,4 +9,5 @@ router = routers.DefaultRouter()
 urlpatterns = [
     # path('', include(router.urls)),
     path('users/register/', views.UserRegisterView.as_view(), name='register_user'),
+    path('users/detail/', views.UserDetailApiView.as_view(), name='user_details'),
 ]
--- post_real/apps/users/views.py
+++ post_real/apps/users/views.py
@@ -1,6 +1,8 @@
-from rest_framework import viewsets, status
-from rest_framework import generics
 from .serializers import UserSerializer
+
+from rest_framework import generics
+from rest_framework import status
+from rest_framework.response import Response
 
 
 class UserRegisterView(generics.CreateAPIView):
@@ -9,3 +11,15 @@ class UserRegisterView(generics.CreateAPIView):
     """
     serializer_class = UserSerializer
     permission_classes = []
+
+
+class UserDetailApiView(generics.GenericAPIView):
+    """
+    View to list details of user.
+    """
+    serializer_class = UserSerializer
+
+    def get (self, request, *args, **kwargs):
+        authenticated_user = request.user
+        serializer = self.serializer_class(authenticated_user)
+        return Response({'success': True, 'data': serializer.data}, status=status.HTTP_200_OK)  
