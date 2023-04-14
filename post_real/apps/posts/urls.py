1b139e9954b44aa6fffa05688406395090c5e723
arunmahara <arunmahara124@gmail.com>
2023-04-14 21:52:49 +0545

    feat: total no. of comments with actual comment and commenter info

--- post_real/apps/posts/urls.py
+++ post_real/apps/posts/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path('like-unlike/<int:postId>/', views.like_unlike_post, name="like_unlike_post"),
     path('like-info/<int:postId>/', views.like_info, name="like_info_of_post"),
-    path('comment/', views.comment_on_post, name="comment_on_post")
+    path('comment/', views.comment_on_post, name="comment_on_post"),
+    path('comment-info/<int:postId>/', views.comment_info, name="comment_info_of_post"),
 ]
