c22ce4af01d29b1d1e822bdd06d1f2479879b875
arunmahara <arunmahara124@gmail.com>
2023-04-29 21:07:17 +0545

    feat: celery beat for deleting expired otp

--- post_real/celery.py
+++ post_real/celery.py
@@ -2,6 +2,7 @@ import os
 from celery import Celery
 from celery.schedules import crontab
 
+
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'post_real.settings')
 
@@ -14,13 +15,23 @@ app = Celery('post_real')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
-app.autodiscover_tasks()
+app.autodiscover_tasks([
+    "post_real.services.tasks.delete_expired_otp",
+    "post_real.services.tasks.send_email"
+])
 
 app.conf.broker_transport_options = {
     'max_retries': 2,
     'interval_start': 0,
     'interval_step': 0.2,
     'interval_max': 0.2,
+}
+
+app.conf.beat_schedule = {
+    'delete_expired_otp_at_24': {
+        'task': 'deleteExpiredOtp',
+        'schedule': crontab(hour=0, minute=0)
+    },
 }
 
 app.conf.timezone = 'Asia/Kathmandu'
