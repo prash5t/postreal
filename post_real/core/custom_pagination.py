a3f46dcf0201ca36a02622e92bbe15cf5131f667
arunmahara <arunmahara124@gmail.com>
2023-04-30 17:31:46 +0545

    feat: pagination on posts list

--- post_real/core/custom_pagination.py
+++ post_real/core/custom_pagination.py
@@ -3,8 +3,17 @@ from rest_framework.pagination import PageNumberPagination
 
 class UserListPagination(PageNumberPagination):
     """
-    Custom PageNumberPagination for user account with page size 50. 
+    Custom PageNumberPagination for listing user account with page size 50. 
     """
     page_size = 50
     page_size_query_param = 'page_size'  
     max_page_size = 50
+
+
+class PostListPagination(PageNumberPagination):
+    """
+    Custom PageNumberPagination for listing posts with page size 20. 
+    """
+    page_size = 20
+    page_size_query_param = 'page_size'  
+    max_page_size = 20
