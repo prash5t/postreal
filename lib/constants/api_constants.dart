aef180404cbba98e13a97a3dbdf7caa1cd45f8e6
aprashantz <prashantghi8@gmail.com>
2023-05-13 20:35:53 +0545

    nothing in this commit: just new branch - yes i am lazy

---
+++ lib/constants/api_constants.dart
@@ -0,0 +1,3 @@
+class APIConstants {
+  static const baseURL = "";
+}
---
+++ lib/data/data_provider/auth_provider.dart
@@ -0,0 +1,9 @@
+class AuthDataProvider {
+  static Future<Map<String, dynamic>> signUpPostReq() async {
+    return {};
+  }
+
+  static Future<Map<String, dynamic>> signInPostReq() async {
+    return {};
+  }
+}
