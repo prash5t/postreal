5eea0a9d8cf7f5d6ed4ed8aff09639e2566837a8
arunmahara <arunmahara124@gmail.com>
2023-05-18 21:16:14 +0545

    feat: ratelimit on generate-token api

--- post_real/core/token_authentication.py
+++ post_real/core/token_authentication.py
@@ -1,12 +1,13 @@
 from django.db.models import Q
 from django.contrib.auth import get_user_model
+from django_ratelimit.decorators import ratelimit
+from django.utils.decorators import method_decorator
 
-from rest_framework import status
-from rest_framework import serializers
+from rest_framework import status, serializers
 from rest_framework_simplejwt.views import TokenViewBase
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
-from post_real.core.log_and_response import generic_response
+from post_real.core.log_and_response import info_logger, generic_response
 from post_real.core.tasks.send_email import email_verification
 
 
@@ -39,6 +40,7 @@ class TokenObtainSerializer(TokenObtainPairSerializer):
         return super().validate(attrs)
 
 
+@method_decorator(ratelimit(key='post:username', rate='3/5m', block=False), name='post')
 class TokenObtainPairView(TokenViewBase):
     """
     Return JWT tokens (access and refresh).
@@ -46,9 +48,18 @@ class TokenObtainPairView(TokenViewBase):
     serializer_class = TokenObtainSerializer
 
     def post(self, request, *args, **kwargs):
+        was_limited = getattr(request, 'limited', False)
+        if was_limited:
+            return generic_response(
+                success=False,
+                message="Limit Exceed! Try Again After 5 Minutes.",
+                status=status.HTTP_403_FORBIDDEN
+            )
         response = super().post(request, *args, **kwargs)
         token = response.data.get('access', None)
         if not token: return response
+        user = request.data.get("username")
+        info_logger.info(f"User: {user} generated access token")
         return generic_response( 
             success=True,
             message='Token Obtained Successfully',
